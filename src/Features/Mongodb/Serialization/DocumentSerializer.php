645ceefd2f568d4b713a2f1a0b5f56d7b866599e
spp28 <sppetrovich@mail.ru>
2025-12-25 21:39:45 +0200

    cast $numberDecimal to float in DocumentSerializer and refactor $regularExpression handling

--- src/Features/Mongodb/Serialization/DocumentSerializer.php
+++ src/Features/Mongodb/Serialization/DocumentSerializer.php
@@ -61,7 +61,7 @@ readonly class DocumentSerializer
             }
 
             if (array_key_exists('$numberDecimal', $value)) {
-                return $value['$numberDecimal'];
+                return (float) $value['$numberDecimal'];
             }
 
             if (array_key_exists('$date', $value)) {
@@ -79,7 +79,8 @@ readonly class DocumentSerializer
             }
 
             if (array_key_exists('$regularExpression', $value)) {
-                return '/' . ($value['$regularExpression']['pattern'] ?? '') . '/' . ($value['$regularExpression']['options'] ?? '');
+                return '/' . ($value['$regularExpression']['pattern'] ?? '')
+                    . '/' . ($value['$regularExpression']['options'] ?? '');
             }
 
             if (array_key_exists('$timestamp', $value)) {
