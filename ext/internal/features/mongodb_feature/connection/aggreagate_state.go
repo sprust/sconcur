e820b3be6477ef544b74c610a81575211e29d126
spp28 <sppetrovich@mail.ru>
2025-12-30 17:20:46 +0200

    add AggregateState to Mongodb feature: encapsulate aggregation flow, handle cursor iteration, and support batch processing (in process)

---
+++ ext/internal/features/mongodb_feature/connection/aggreagate_state.go
@@ -0,0 +1,97 @@
+package connection
+
+import (
+	"context"
+	"sconcur/internal/dto"
+	"sconcur/internal/features/mongodb_feature/helpers"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type AggregateState struct {
+	ctx         context.Context
+	message     *dto.Message
+	mCollection *mongo.Collection
+	pipeline    interface{}
+	batchSize   int
+	cursor      *mongo.Cursor
+}
+
+func NewAggregateState(
+	ctx context.Context,
+	message *dto.Message,
+	mCollection *mongo.Collection,
+	pipeline interface{},
+	batchSize int,
+) *AggregateState {
+	return &AggregateState{
+		ctx:         ctx,
+		message:     message,
+		mCollection: mCollection,
+		pipeline:    pipeline,
+		batchSize:   batchSize,
+	}
+}
+
+func (a *AggregateState) Next() *dto.Result {
+	if a.cursor == nil {
+		cursor, err := a.mCollection.Aggregate(a.ctx, a.pipeline)
+
+		if err != nil {
+			return dto.NewErrorResult(
+				a.message,
+				errFactory.ByErr("aggregate error", err),
+			)
+		}
+
+		a.cursor = cursor
+	}
+
+	var items []interface{}
+
+	for a.cursor.Next(a.ctx) {
+		if err := a.cursor.Err(); err != nil {
+			_ = a.cursor.Close(a.ctx)
+
+			return dto.NewErrorResult(
+				a.message,
+				errFactory.ByErr("aggregate cursor error", err),
+			)
+		}
+
+		items = append(items, a.cursor.Current)
+
+		if len(items) == a.batchSize {
+			response, err := helpers.MarshalDocument(
+				bson.D{
+					{Key: resultKey, Value: items},
+				},
+			)
+
+			if err != nil {
+				return dto.NewErrorResult(
+					a.message,
+					errFactory.ByErr("aggregate result marshal error", err),
+				)
+			}
+
+			return dto.NewSuccessResultWithNext(a.message, response)
+		}
+	}
+
+	response, err := helpers.MarshalDocument(
+		bson.D{
+			{Key: resultKey, Value: items},
+		},
+	)
+
+	if err != nil {
+		return dto.NewErrorResult(
+			a.message,
+			errFactory.ByErr("aggregate result marshal error", err),
+		)
+	}
+
+	return dto.NewSuccessResult(a.message, response)
+}
